ff95e39f8b305a685463b353678cb504aec47eb0
Yura <idmi@sfedu.ru>
2023-07-29 02:01:05 +0300

    Fixed run tests, added mac helper

---
+++ bin/tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the PYTHONPATH to the relative path of the factory_analyzer directory
+cd ..
+PYTHONPATH=$(dirname "$PWD")
+
+# Run the Python test using the relative path to the virtual environment's Python interpreter
+sudo -E venv/bin/python -m pytest -v -s
--- tests/services/test_cyber_factory_services.py
+++ tests/services/test_cyber_factory_services.py
@@ -1,13 +1,28 @@
 import pytest
-import json
 
 from src.services.cyber_factory_service import CyberFactoryService
 from tests.helpers.random_ip import random_ipv4 as random_ip
+from tests.helpers.random_mac import get_random_mac_address as random_mac
 
 
 @pytest.fixture
 def obj():
     return CyberFactoryService()
+
+
+@pytest.fixture
+def ip():
+    return random_ip()
+
+
+@pytest.fixture
+def ips():
+    return random_ip(count=2)
+
+
+@pytest.fixture
+def mac():
+    return random_mac()
 
 
 def test_get_url(obj):
@@ -22,27 +37,24 @@ def test_login(obj):
     assert result == expected_result
 
 
-def test_send_one_network_interface(obj):
+def test_send_one_network_interface(obj, ip):
     expected_result = [200, 201]
     # expected_result = None
-    result = obj.send_network_interface({"name": "eth11", "ipAddress": "192.168.1.102"})
+    result = obj.send_network_interface({"name": "eth11", "ipAddress": ip})
     assert result in expected_result
 
 
-def test_send_list_network_interface(obj):
+def test_send_list_network_interface(obj, ips):
     expected_result = [200, 201]
-    ips = random_ip(count=2)
     result = obj.send_list_network_interfaces([{"name": "eth55", "ipAddress": ips[0]},
                                                {"name": "eth51", "ipAddress": ips[1]}])
     assert result in expected_result
 
 
-def test_send_arp_record(obj):
-    ip = random_ip()
-    response = obj.send_arp_record({"ipAddress": ip, "macAddress": "00:00:00:00:00"})
+def test_send_arp_record(obj, ip, mac):
+    response = obj.send_arp_record({"ipAddress": ip, "macAddress": mac})
 
     assert response["id"] is not None
 
     assert response["ipAddress"] == ip
-    assert response["macAddress"] == "00:00:00:00:00"
-
+    assert response["macAddress"] == mac
